271c8e0ecddfa387e1b0316c9eb702a3ee1276b4
oleg.mulya30@gmai.coml <oleg.mulya30@gmai.com>
2022-05-16 15:56:05 +0300

    Added test marker on dnepr

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 let map;
 
 function initMap() {
+
+  const dnepr = { lat: 48.4775495409843, lng: 35.02095581604529 }
+
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 48.4775495409843, lng: 35.02095581604529 },
+    center: dnepr,
     zoom: 8,
   });
+
+  const marker = new google.map.Marker({
+    position: dnepr,
+    map: map
+  })
 }
 
 window.initMap = initMap;
