ae38bf364f72d5bd3b7336b2a8c600df23c848f8
oleg.mulya30@gmai.coml <oleg.mulya30@gmai.com>
2022-05-17 20:28:31 +0300

    Added data, try parse xls

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 let map;
 
+var ExcelToJSON = function() {
+
+  this.parseExcel = function(file) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      var data = e.target.result;
+      var workbook = XLSX.read(data, {
+        type: 'binary'
+      });
+
+      workbook.SheetNames.forEach(function(sheetName) {
+        // Here is your object
+        var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+        var json_object = JSON.stringify(XL_row_object);
+        console.log(json_object);
+
+      })
+
+    };
+
+    reader.onerror = function(ex) {
+      console.log(ex);
+    };
+
+    reader.readAsBinaryString(file);
+  };
+};
+
 function initMap() {
 
   const dnepr = { lat: 48.4775495409843, lng: 35.02095581604529 }
@@ -9,10 +38,7 @@ function initMap() {
     zoom: 8,
   });
 
-  const marker = new google.maps.Marker({
-    position: dnepr,
-    map: map
-  })
+  ExcelToJSON.parseExcel("data.xls")
 }
 
 window.initMap = initMap;
