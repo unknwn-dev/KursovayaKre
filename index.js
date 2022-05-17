edd53a633de6b6a4c0e16342eb16be9eae00b978
oleg.mulya30@gmai.coml <oleg.mulya30@gmai.com>
2022-05-17 19:14:13 +0300

    map is constant

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function initMap() {
 
   const dnepr = { lat: 48.4775495409843, lng: 35.02095581604529 }
 
-  map = new google.maps.Map(document.getElementById("map"), {
+  const map = new google.maps.Map(document.getElementById("map"), {
     center: dnepr,
     zoom: 8,
   });
