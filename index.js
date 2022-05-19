0120ab1940dee0959bd8efc38d39094a01f0785e
oleg.mulya30@gmai.coml <oleg.mulya30@gmai.com>
2022-05-19 18:38:57 +0300

    points generations

--- index.js
+++ index.js
@@ -4,23 +4,30 @@ function initMap() {
 
   const dnepr = { lat: 48.4775495409843, lng: 35.02095581604529 }
   const zone = { 
-    latMin: 48.4526,
-    latMax: 48.46859,
-    lngMin: 34.98936,
-    lngMax: 34.99
+    latMin: 48.6,
+    latMax: 48.3,
+    lngMin: 34.7,
+    lngMax: 35.2
   }
 
   const map = new google.maps.Map(document.getElementById("map"), {
     center: dnepr,
-    zoom: 8,
+    zoom: 9,
   });
 
+  var points = new Array();
+
+  for(var i = 0; i < numOfPoints; i++){
+    points.push({
+      lat : Math.random() * (zone.latMax - zone.latMin) + zone.latMin,
+      lng : Math.random() * (zone.lngMax - zone.lngMin) + zone.lngMin
+    });
+  }
+
+  
   for(var i = 0; i < numOfPoints; i++){
     var point = new google.maps.Marker({
-      position : {
-        lat : Math.random() * (zone.latMax - zone.latMin) + zone.latMin,
-        lng : Math.random() * (zone.lngMax - zone.lngMin) + zone.lngMin
-      },
+      position : points[i],
       map : map
     });
   }
